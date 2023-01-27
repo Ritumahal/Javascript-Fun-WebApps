ad565c6e9ec2a105c22d3566354c90cb4fe60443
Ritu <rmahal@raveinfosys.com>
2023-01-27 21:00:59 +0530

    created webapp for translation

---
+++ web/app.js
@@ -0,0 +1,31 @@
+var textToTranslate = document.querySelector("#textToTranslate");
+var btnTranslate = document.querySelector("#btnTranslate");
+btnTranslate.addEventListener('click',translate);
+var outputDiv = document.querySelector("#outputDiv");
+//fun translations
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
+//fun kanye quotes
+//var serverURL = "https://api.kanye.rest/";
+function errHandle(error){
+	console.log(error);
+}
+function getTranslatedData(text){
+	return serverURL+"?"+"text="+text;
+}
+function translate() {	
+	fetch(getTranslatedData(textToTranslate.value))
+	.then(response => response.json())
+	.then(output => {
+		console.log(output)
+		//fun translation
+		var outputTxt = output.contents.translated;
+		console.log(outputTxt)
+		outputDiv.innerHTML = outputTxt;
+
+		//fun kanye quotes
+		//outputDiv.innerHTML = output.quote;
+		})
+	.catch(errHandle);
+	
+}
+
